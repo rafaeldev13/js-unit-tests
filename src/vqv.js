7f777c141c994c645384b529275d074545313b5e
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-05-14 10:22:39 -0400

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = () => {};
-
+const vqv = (nome, idade) => {
+  if (nome === undefined && idade === undefined) {
+    return undefined;
+  }
+ let frase = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+ return frase;
+};
+console.log(vqv('Tunico', 29));
 module.exports = vqv;
